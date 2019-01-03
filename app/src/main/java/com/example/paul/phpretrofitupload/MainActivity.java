e2022db3f9c59ac2ffa1d6e0b91844015fbb7c28
paul <ztt1995120@gmail.com>
2019-01-03 21:42:06 +0800

    1.3 add: Utils, for progress update UI, a callback

--- app/src/main/java/com/example/paul/phpretrofitupload/MainActivity.java
+++ app/src/main/java/com/example/paul/phpretrofitupload/MainActivity.java
@@ -1,14 +1,18 @@
 package com.example.paul.phpretrofitupload;
 
 import android.Manifest;
+import android.app.Activity;
 import android.content.Intent;
 import android.content.pm.PackageManager;
+import android.net.Uri;
+import android.support.annotation.NonNull;
 import android.support.v4.app.ActivityCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageView;
+import android.widget.Toast;
 
 import com.example.paul.phpretrofitupload.Remote.IUploadAPI;
 import com.example.paul.phpretrofitupload.Remote.RetrofitClient;
@@ -24,6 +28,8 @@ public class MainActivity extends AppCompatActivity {
 
     Button btnUpload;
     ImageView imageView;
+
+    Uri selectedFileUri;
 
     private IUploadAPI getAPIUpload() {
         return RetrofitClient.getClient(BASE_URL).create(IUploadAPI.class);
@@ -52,6 +58,41 @@ public class MainActivity extends AppCompatActivity {
             }
         });
 
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        switch (requestCode) {
+            case REQUEST_PERMISSION:
+                if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                    Toast.makeText(this, "permission granted", Toast.LENGTH_SHORT).show();
+                }
+                else {
+                    Toast.makeText(this, "permission denied", Toast.LENGTH_SHORT).show();
+                }
+        }
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (resultCode == Activity.RESULT_OK) {
+            if (requestCode == PICK_FILE_REQUEST) {
+                if (data == null) {
+                    return;
+                }
+                else {
+                    selectedFileUri = data.getData();
+                    if (selectedFileUri != null && !selectedFileUri.getPath().isEmpty()) {
+                        imageView.setImageURI(selectedFileUri);
+                    }
+                    else {
+                        Toast.makeText(this, "Can't upload file to server, data is null", Toast.LENGTH_SHORT).show();
+                    }
+                }
+            }
+        }
     }
 
     private void chooseFile() {
---
+++ app/src/main/java/com/example/paul/phpretrofitupload/Utils/UploadCallbacks.java
@@ -0,0 +1,11 @@
+package com.example.paul.phpretrofitupload.Utils;
+
+/**
+ * Created by paul on 2019/1/3
+ * last modified at 21:29.
+ * Desc:
+ */
+
+interface UploadCallbacks {
+    void onProgressUpdate(int percentage);
+}
